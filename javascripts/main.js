65d3370aca619f04f1bbaee88cb141edadcb9657
Tim Turner <timturner154@gmail.com>
2013-02-07 15:27:08 -0600

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,1 @@
 console.log('This would be the main JS file.');
-<script src='http://gitlive.com/githublive.min.js'></script>
-<script>var GithubPush = {num_old:5,nodes:['f1vefour/supernatural-kernel']}</script>
-<div id='commits'></div>
